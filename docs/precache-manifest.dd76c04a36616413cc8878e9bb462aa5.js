f0246566f87d5475870fec54344b0eea0869b755
Roger Sobrado <roger.sobrado.e@gmail.com>
2020-08-07 22:45:28 -0600

    css variable and darkmode refactor

---
+++ docs/precache-manifest.dd76c04a36616413cc8878e9bb462aa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31116e0adbe1b01e973372d442906bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a283a4620932561b712",
+    "url": "/static/css/main.ffc3ca7a.chunk.css"
+  },
+  {
+    "revision": "6f0bbb5ca9fef5bdbeda",
+    "url": "/static/js/2.b77f4536.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b77f4536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a283a4620932561b712",
+    "url": "/static/js/main.ba46812c.chunk.js"
+  },
+  {
+    "revision": "6af8a1952490bd99291d",
+    "url": "/static/js/runtime-main.7cdd4e55.js"
+  }
+]);
